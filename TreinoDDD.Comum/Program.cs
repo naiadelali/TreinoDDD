287fd68aeb87e8793251490586337d0c4898d1bf
Fábio Pereira de Souza <fabiopds11@gmail.com>
2015-08-20 20:51:46 -0300

    Incluído estrutura de validação

---
+++ TreinoDDD.Comum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinoDDD.Comum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
